e56384371a27605836813572e0daef475d374c7f
Anton Hladkyi <gladkyy@ukr.net>
2019-10-28 20:30:59 +0200

    added some functions

---
+++ Algo_k-ary_heap/Algo_k-ary_heap/Heap.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <iostream>
+#include <vector>
+class Heap
+{
+private:
+	std::vector<int> heap;
+	int multiplicity;
+
+public:
+	Heap();
+	~Heap();
+};
+
---
+++ Algo_segregate_by_key/Algo_segregate_by_key/Algo_segregate_by_key.cpp
@@ -0,0 +1,38 @@
+﻿#include "pch.h"
+#include <iostream>
+using namespace std;
+
+/*Function to put all 0s on left and all 1s on right*/
+void segregate0and1(pair<int,int> arr[],
+	int size)
+{
+	int type0 = 0;
+	int type1 = size - 1;
+
+	while (type0 < type1)
+	{
+		if (arr[type0].first == 1)
+		{
+			swap(arr[type0],
+				arr[type1]);
+			type1--;
+		}
+		else
+			type0++;
+	}
+}
+
+/* Driver code */
+int main()
+{
+	pair<int,int> p(1,0);
+	pair<int, int>  arr[] = { pair<int,int>(0,3), pair < int,int> (1,4), pair < int,int> (0,6), pair < int,int> (1,15), pair < int,int> (1,1), pair < int,int> (1,5),pair<int,int>(0,7) };
+	int i, arr_size = sizeof(arr) / sizeof(arr[0]);
+
+	segregate0and1(arr, arr_size);
+
+	cout << "Array after segregation ";
+	for (i = 0; i < 6; i++)
+		cout << arr[i].second << " ";
+	return 0;
+}
